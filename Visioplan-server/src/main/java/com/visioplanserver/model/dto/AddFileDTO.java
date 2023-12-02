10d8a4b80d8bb2d2bd00d8268511cf2d4e98a2d9
Mihail Slavov <mihail_slavov@icloud.com>
2023-12-02 17:39:01 +0200

    html cleaning and some new validations

--- Visioplan-server/src/main/java/com/visioplanserver/model/dto/AddFileDTO.java
+++ Visioplan-server/src/main/java/com/visioplanserver/model/dto/AddFileDTO.java
@@ -4,6 +4,7 @@ import com.visioplanserver.model.entity.enums.BuldingDocumentationPartEnum;
 import com.visioplanserver.model.entity.enums.DrawingTypeEnum;
 import com.visioplanserver.model.entity.enums.FileExtensionEnum;
 import com.visioplanserver.model.entity.enums.TextFileTypeEnum;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,20 +12,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 public record AddFileDTO(
         @NotNull
-        @Size(min = 2, max = 250, message = "Building name must be between 2 and 40 characters!")
+        @Size(min = 2, max = 250, message = "Please choose a building!")
         String buildingName,
         @NotNull
         String floor,
-        @NotNull
+        @NotNull(message = "Please choose a part of the building!")
         BuldingDocumentationPartEnum part,
-
         DrawingTypeEnum drawingType,
-
         TextFileTypeEnum textFileType,
-        @NotNull
+        @NotBlank(message = "Please enter a name!")
         @Size(min = 2, max = 250, message = "Name must be between 2 and 40 characters!")
         String name,
-
         FileExtensionEnum extension,
         @NotNull
         MultipartFile dataFile) {
